fd4004c85ab0ab25fd6193edc7544c76c33fb1d5
Aldo <aldo.martini93@gmail.com>
2018-06-25 18:00:11 -0400

    debugging

--- proc.c
+++ proc.c
@@ -615,7 +615,7 @@ getprocs()
 
 int addr_translate(void* vaddr)
 {
-      cprintf("vaddr = %p\n",vaddr);
+ cprintf("vaddr = %p\n",vaddr);
  int paddr;
  pde_t *pgdir;
  pte_t *pgtab;
@@ -623,7 +623,7 @@ int addr_translate(void* vaddr)
  pte_t *pte;
 
  pgdir = myproc()->pgdir
- //cprintf("page directory base is: %p\n",myproc()->pgdir);
+ cprintf("page directory base is: %p\n",myproc()->pgdir);
  pde = &pgdir[PDX(vaddr)];
  if(*pde & PTE_P){
  pgtab = (pte_t*)P2V(PTE_ADDR(*pde));
